8443ca2a5843e24529c99c2d69ed356d971f99c8
Victor L <rhettb198501@hotmail.com>
2020-12-21 23:19:10 -0300

    fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from sampling import app
 
 if __name__ == "__main__":
-    app.run(debug = False, host="0.0.0.0", port="8080", use_reloader=False,)
+    app.run(debug = False, host="0.0.0.0", port="9080", use_reloader=False,)
